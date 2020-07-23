239399dce134c3bf103b72bd1efad5c4bd725ddb
Narajeff <Narajeff@users.noreply.github.com>
2020-07-23 11:47:32 -0300

    agregando el fullcalendar

---
+++ src/public/app.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var calendarEl = document.getElementById('calendar');
+
+    var calendar = new FullCalendar.Calendar(calendarEl, {
+        headerToolbar: {
+            left: 'prev,next today',
+            center: 'title',
+            right: 'dayGridMonth,timeGridWeek,timeGridDay'
+        }
+
+    });
+
+    calendar.render();
+});
