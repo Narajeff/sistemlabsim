9cb4fb5669cbf125df7c46e36f053fa070c013da
Narajeff <46496002+Narajeff@users.noreply.github.com>
2023-10-28 21:02:04 -0300

    Update database.js

--- src/database.js
+++ src/database.js
@@ -1,10 +1,12 @@
 const mongoose = require('mongoose');
- mongoose.connect('mongodb+srv://<narajeff>:<Seruno774*>@cluster0.ymqt7.mongodb.net/?retryWrites=true&w=majority/notes-db-app',{
-//mongoose.connect('mongodb+srv://<narajeff>:<narajeff>@cluster0.ymqt7.mongodb.net/?retryWrites=true&w=majority', {
-///*//mongoose.connect('mongodb://localhost/notes-db-app', {*/
+mongoose.connect('mongodb+srv://vercel-admin-user:narajeff@cluster0.ymqt7.mongodb.net/notes-db-app',{
+//mongoose.connect('mongodb+srv://narajeff:<Seruno774*>@cluster0.ymqt7.mongodb.net/test',{
+//mongoose.connect('mongodb://127.0.0.1:27017/notes-db-app', {
+ //mongoose.connect('mongodb+srv://<sathyanarayanajiki@gmail.com>:<Seruno774*>@cluster0.ymqt7.mongodb.net/?retryWrites=true&w=majority/notes-db-app',{
         useCreateIndex: true,
         useNewUrlParser: true,
         useFindAndModify: false
     })
     .then(db => console.log('DB is conected'))
     .catch(err => console.error(err));
+
