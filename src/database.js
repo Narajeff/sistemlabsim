79b0ba73327724b29ca326cde2268d3defa12ca2
Narajeff <46496002+Narajeff@users.noreply.github.com>
2025-06-26 20:06:25 -0300

    Update database.js
    Probando de nuevo la base de datos ya que mongo habia cerrado las conexiones viejas

--- src/database.js
+++ src/database.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://vercel-admin-user:narajeff@cluster0.ymqt7.mongodb.net/',{
+mongoose.connect('mongodb+srv://vercel-admin-user:Seruno774@cluster0.lqregut.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',{
 //mongoose.connect('mongodb+srv://narajeff:<Seruno774*>@cluster0.ymqt7.mongodb.net/test',{
 //mongoose.connect('mongodb://127.0.0.1:27017/notes-db-app', {
  //mongoose.connect('mongodb+srv://<sathyanarayanajiki@gmail.com>:<Seruno774*>@cluster0.ymqt7.mongodb.net/?retryWrites=true&w=majority/notes-db-app',{
