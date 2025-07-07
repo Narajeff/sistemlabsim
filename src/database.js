cb268f46e02999e881ad4071e72c03a89c821503
Narajeff <46496002+Narajeff@users.noreply.github.com>
2025-07-07 19:27:17 -0300

    Update database.js

--- src/database.js
+++ src/database.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://vercel-admin-user:Seruno774@cluster0.lqregut.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',{
-//mongoose.connect('mongodb+srv://narajeff:<Seruno774*>@cluster0.ymqt7.mongodb.net/test',{
+mongoose.connect('mongodb+srv://vercel-admin-user:nodegood@cluster0.lqregut.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',{
+//mongoose.connect('mongodb+srv://narajeff:<nodegood*>@cluster0.ymqt7.mongodb.net/test',{
 //mongoose.connect('mongodb://127.0.0.1:27017/notes-db-app', {
- //mongoose.connect('mongodb+srv://<sathyanarayanajiki@gmail.com>:<Seruno774*>@cluster0.ymqt7.mongodb.net/?retryWrites=true&w=majority/notes-db-app',{
+ //mongoose.connect('mongodb+srv://<sathyanarayanajiki@gmail.com>:<nodegood*>@cluster0.ymqt7.mongodb.net/?retryWrites=true&w=majority/notes-db-app',{
         useCreateIndex: true,
         useNewUrlParser: true,
         useFindAndModify: false
