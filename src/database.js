e6a39fe4f6733221626d2a8a72cfe71b1ccb34a7
Narajeff <46496002+Narajeff@users.noreply.github.com>
2025-08-07 19:52:07 -0300

    Update database.js
    conectando atlas mongo

--- src/database.js
+++ src/database.js
@@ -1,10 +1,8 @@
 const mongoose = require('mongoose');
-//mongoose.connect('mongodb+srv://vercel-admin-user:nodegood@cluster0.lqregut.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',{
+mongoose.connect(process.env.MONGO_URI, {
 //mongoose.connect('mongodb+srv://narajeff:<nodegood*>@cluster0.ymqt7.mongodb.net/test',{
 //mongoose.connect('mongodb://127.0.0.1:27017/notes-db-app', {
  //mongoose.connect('mongodb+srv://<sathyanarayanajiki@gmail.com>:<nodegood*>@cluster0.ymqt7.mongodb.net/?retryWrites=true&w=majority/notes-db-app',{
-   mongodb+srv://narajeff:narajeff@cluster0.lqregut.mongodb.net/
-
         useCreateIndex: true,
         useNewUrlParser: true,
         useFindAndModify: false,
